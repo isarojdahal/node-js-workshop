a1015e51da6213934e6b24271a167a981cc39da3
Saroj Dahal <isarojdahal@gmail.com>
2022-05-11 08:44:53 +0545

    added: day3 dir

---
+++ day3/filesystem.js
@@ -0,0 +1,19 @@
+import * as fs from "fs/promises";
+
+// await fs.writeFile(
+//   "hello.txt",
+//   "Thankyou InternSathi for being our Intern Partner"
+// );
+
+// try {
+//   let contents = await fs.readFile("hello.txt", "utf-8");
+//   console.log(contents);
+// } catch (err) {
+//   console.log(err);
+// }
+// console.log("exited");
+
+//unlink : deletes the file.
+// await fs.unlink("a.txt");
+
+// await fs.rmdir("test");
---
+++ day3/url.js
@@ -0,0 +1,6 @@
+import { URL } from "url";
+
+const url = new URL("https://google.com/huddleNepal");
+
+console.log(url.protocol);
+console.log(url.hostname);
