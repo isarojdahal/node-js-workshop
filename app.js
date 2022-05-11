cd90e61bf8b3c719c60956708670d9375eb75f74
Saroj Dahal <isarojdahal@gmail.com>
2022-05-11 08:45:57 +0545

    fixed: untracked files

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("hello from app.js");
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a > b ? a - b : b - a;
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+// // console.log("hello from node");
+
+// // console.log(2 == 4);
+
+// // function add(a, b) {
+// //   return a + b;
+// // }
+
+// // console.log(add(1, 2));
+
+// companyName = "Cliff Byte";
+// eventPartner = "Huddle Nepal";
+
+// function print() {
+//   var abc = "abc";
+//   console.log(companyName);
+//   console.log(eventPartner);
+// }
+
+// print();
+// console.log(abc);
+
+// console.log(process.env.USER);
+
+// setInterval(() => console.log("i am printed after 2 sec"), 2000);
+
+// console.log(__filename);
+// console.log(__dirname);
+
+// require("");
+
+// const { add, sub } = require("./app");
+
+// import { add, sub } from "./app.js";
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+
+// console.log(import.meta.url);
