e0d20e2b183d1ded32a71cb062eee7fdc2be17be
Saroj Dahal <isarojdahal@gmail.com>
2022-05-22 15:00:26 +0545

    added: day 9 dir

---
+++ day9/controllers/adminController.js
@@ -0,0 +1,2 @@
+// import UserController from "./userController";
+// class AdminController {}
--- day9/routes/userRoute.js
+++ day9/routes/userRoute.js
@@ -1,106 +1,23 @@
 import express from "express";
-import connection from "../models/index.js";
-import userModel from "../models/userModel.js";
-import { Op } from "sequelize";
+import UserController from "../controllers/userController.js";
 
 const router = express.Router();
+const userController = new UserController();
 
 //inserts user to the database (CREATE)
-router.post("/add", async (req, res) => {
-  // const { username, location } = req.body;
-
-  try {
-    // const data = await connection.query(
-    //   `INSERT INTO users(username,location) VALUES(?,?),('')''`,
-    //   [username, location]
-    // );
-
-    // const data = await userModel.bulkCreate(req.body);
-
-    const data = await userModel.create(req.body);
-
-    console.log(data);
-    res.status(200).json(data);
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.post("/add", userController.addUser);
 
 //read operation (Read)
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  if (id) {
-    const data = await userModel.findByPk(id);
-    console.log(data);
-
-    if (data) {
-      res.json(data);
-    } else res.json([]);
-  } else res.json({ success: false, message: "User ID NOt provided" });
-});
+router.get("/:id", userController.getUserByID);
 
 //update the users
 
-router.put("/update/:id", async (req, res) => {
-  const { id } = req.params;
-
-  if (id) {
-    //
-
-    const { username, location } = req.body;
-
-    const data = await userModel.update(
-      { username, location },
-      {
-        where: {
-          id,
-        },
-      }
-    );
-
-    console.log(data);
-
-    if (data[0]) res.json({ success: true, message: "User Updated" });
-    else res.json({ success: false, message: "Unable to update User" });
-  } else res.json({ success: false, message: "User ID Not provided" });
-});
+router.put("/update/:id", userController.updateUser);
 
 //delete.
-router.delete("/delete/:id", async (req, res) => {
-  const { id } = req.params;
-
-  if (id) {
-    //
-
-    const data = await userModel.destroy({
-      where: {
-        id,
-      },
-    });
-
-    console.log(data);
-    if (data) res.json({ success: true, message: "user deleted successfully" });
-    else res.json({ success: false, message: "couldn't delete user" });
-  } else {
-    res.status(403).json({ success: false, message: "User ID Not provided" });
-  }
-});
+router.delete("/delete/:id", userController.deleteUser);
 
 // . localhost:8000/user/search/by?location=ktm
-router.get("/search/by", async (req, res) => {
-  const { location } = req.query;
-
-  const data = await userModel.findAll({
-    where: {
-      location: {
-        [Op.like]: `%${location}%`,
-      },
-      limit: 10,
-    },
-  });
-
-  console.log(data);
-  res.json(data);
-});
+router.get("/search/by", userController.searchUserByLocation);
 
 export default router;
